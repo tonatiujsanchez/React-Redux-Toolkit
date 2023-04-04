1116f296f57849ce36c6114db09c74b166096563
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-04-03 22:32:30 -0600

    Configuracion e Intalaciones de Redux implementados

---
+++ src/PokemonApp.jsx
@@ -0,0 +1,14 @@
+
+export const PokemonApp = () => {
+    return (
+        <>
+            <h1>Pokemon app</h1>
+            <hr />
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+                <li>item 3</li>
+            </ul>
+        </>
+    )
+}
